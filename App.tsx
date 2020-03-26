c2d628d72a3b3759efc8889cb66ec5434ef25cdb
Mike Etlinger <mk.etlinger@gmail.com>
2020-03-26 12:50:11 -0400

    add test Button

--- App.tsx
+++ App.tsx
@@ -1,10 +1,44 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+
+function HelpRequestForm() {
+  const [value, onChangeText] = useState('');
+
+  function handleOnSubmit(e) {
+    console.log(e);
+  }
+
+  return (
+    <View>
+      <Text>Name</Text>
+      <TextInput 
+        value={value}
+        onChangeText={(text) => onChangeText(text) }
+        placeholder="Placeholder text"
+      />
+      <Text>Contact Number</Text>
+      <TextInput 
+        value={value}
+        onChangeText={(text) => onChangeText(text) }
+        placeholder="Placeholder text"
+      />
+
+      <Button
+        title="SUBMIT"
+        color="#f194ff"
+        onPress={handleOnSubmit}
+      />
+    </View>
+  )
+}
 
 export default function App() {
+  
+
   return (
     <View style={styles.container}>
-      <Text>Testing!</Text>
+      {/* <Text>Testing!</Text> */}
+      <HelpRequestForm />
     </View>
   );
 }
