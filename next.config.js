24e73970ee177d6b6c5cfa4f4088db170870a6d4
Mike Etlinger <mk.etlinger@gmail.com>
2020-03-26 20:21:04 -0400

    update env variables

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = withCSS({
   env: {
-    HOST: isProd ? '' : 'http://localhost:3000',
-    API_URL: isProd ? '' : '/api',
+    HOST: isProd ? 'https://pair-up.now.sh' : 'http://localhost:3000',
+    API_URL: '/api',
   },
 })
