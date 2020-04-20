2777e01c502c02e1574343eaa42779ef463b6aab
Mike Etlinger <mk.etlinger@gmail.com>
2020-04-19 20:33:47 -0400

    add environment variables to .env

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
-const withCSS = require('@zeit/next-css')
-const isProd = process.env.NODE_ENV === 'production'
+const withCSS = require('@zeit/next-css');
+const Dotenv = require("dotenv-webpack");
+const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = withCSS({
+  webpack: (config) => {
+    config.plugins.push(new Dotenv({ silent: true }));
+
+    return config;
+  },
   env: {
     HOST: isProd ? 'https://pair-up.now.sh' : 'http://localhost:3000',
-    API_URL: '/api',
   },
 })
