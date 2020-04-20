e3c6905f8729bdbb3328d22e2d7307cda9061671
Mike Etlinger <mk.etlinger@gmail.com>
2020-04-19 20:36:02 -0400

    abstract apollo client out into hasura service, add geolocation matching

--- pages/api/requests/create/index.js
+++ pages/api/requests/create/index.js
@@ -1,67 +1,34 @@
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import { ApolloClient } from 'apollo-client';
-import { createHttpLink } from 'apollo-link-http';
-import gql from 'graphql-tag';
-
-
-const defaultOptions = {
-  watchQuery: {
-    fetchPolicy: 'cache-and-network',
-    errorPolicy: 'ignore',
-  },
-  query: {
-    fetchPolicy: 'network-only',
-    errorPolicy: 'all',
-  },
-  mutate: {
-    errorPolicy: 'all'
-  },
-}
-
-const client = new ApolloClient({
-  link: createHttpLink({
-    uri: 'https://pair-up-graphql.herokuapp.com/v1/graphql',
-    headers: {
-      'x-hasura-admin-secret': 'oP#kJ2rUShV%Jw!zUQSjO!30f9UsKS&!RYztv*g',
-      'x-hasura-role': 'admin',
-    },
-  }),
-  defaultOptions: defaultOptions,
-  cache: new InMemoryCache()
-})
+import { getLatLngFromAddress } from '../../services/google';
+import { createRequest, getVolunteersNearRequester } from '../../services/hasura';
 
 export default async (req, res ) => {
   const { name, address, phoneNumber, email, requestDetails } = JSON.parse(req.body);
-  
+  // @TODO: Add validations
   try {
-    const response = await client.mutate({
-      mutation: gql`
-      mutation {
-        insert_requests(objects: [
-          {
-            name: "${name}",
-            address: "${address}",
-            phoneNumber: "${phoneNumber}",
-            email: "${email}",
-            requestDetails: "${requestDetails}",
-            geolocation: "40.688617,-73.977499",
-          }
-        ]) {
-          affected_rows
-          returning {
-            id
-            name
-            phoneNumber
-            email
-            address
-            requestDetails
-          }
-        }
-      }
-      `
-    })
+    const { latitude, longitude } = await getLatLngFromAddress(address);
+    
+    const volunteerResponse = await getVolunteersNearRequester({
+      latitude,
+      longitude,
+    });
 
-    console.log(response);
+    console.log(volunteerResponse.data);
+
+    const response = await createRequest({
+      name,
+      address,
+      phoneNumber,
+      email,
+      requestDetails,
+      latitude,
+      longitude,
+    });
+
+    if (response.errors) {
+      console.log(response.errors[0].extensions);
+      throw new Error(response.errors[0].message);
+    }
+
     res.setHeader('Content-Type', 'application/json');
     res.statusCode = 200;
     res.end();
--- pages/api/volunteers/create/index.js
+++ pages/api/volunteers/create/index.js
@@ -1,68 +1,25 @@
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import { ApolloClient } from 'apollo-client';
-import { createHttpLink } from 'apollo-link-http';
-import gql from 'graphql-tag';
-
-
-const defaultOptions = {
-  watchQuery: {
-    fetchPolicy: 'cache-and-network',
-    errorPolicy: 'ignore',
-  },
-  query: {
-    fetchPolicy: 'network-only',
-    errorPolicy: 'all',
-  },
-  mutate: {
-    errorPolicy: 'all'
-  },
-}
-
-const client = new ApolloClient({
-  link: createHttpLink({
-    uri: 'https://pair-up-graphql.herokuapp.com/v1/graphql',
-    headers: {
-      'x-hasura-admin-secret': 'oP#kJ2rUShV%Jw!zUQSjO!30f9UsKS&!RYztv*g',
-      'x-hasura-role': 'admin',
-    },
-  }),
-  defaultOptions: defaultOptions,
-  cache: new InMemoryCache()
-})
+import { getLatLngFromAddress } from '../../services/google';
+import { createVolunteer } from '../../services/hasura';
 
 export default async (req, res ) => {
   const { name, address, phoneNumber, email } = JSON.parse(req.body);
-  
+  // @TODO: Add validations
   try {
-    const response = await client.mutate({
-      mutation: gql`
-      mutation {
-        insert_volunteers(objects: [
-          {
-            name: "${name}",
-            address: "${address}",
-            phoneNumber: "${phoneNumber}",
-            email: "${email}",
-            geolocation: "(40.688617,-73.977499)",
-          }
-        ]) {
-          affected_rows
-          returning {
-            id
-            name
-            phoneNumber
-            email
-            address
-          }
-        }
-      }
-      `
-    })
+    const { latitude, longitude } = await getLatLngFromAddress(address);
+
+    const response = createVolunteer({
+      name,
+      address,
+      phoneNumber,
+      email,
+      latitude,
+      longitude, 
+    });
+   
     if (response.errors) { 
       throw new Error('Error inserting into database: ' + JSON.stringify(response.errors)) 
     }
 
-    console.log(response);
     res.setHeader('Content-Type', 'application/json');
     res.statusCode = 200;
     res.end();
@@ -70,6 +27,6 @@ export default async (req, res ) => {
   catch (error) {
     console.error(error);
     res.statusCode = 500;
-    res.end(JSON.stringify({ error: error.message }));
+    res.end();
   }
 };
